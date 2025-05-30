f3ab6b18b4e6fc2b0e3dd18f8126b1a1db0aabb1
Александра Вершинина <xx.vershinina.xx@yandex.ru>
2025-05-30 19:59:45 +0300

    c

--- my-app/src/main/java/com/mycompany/app/Task3.java
+++ my-app/src/main/java/com/mycompany/app/Task3.java
@@ -26,7 +26,6 @@ public class Task3 {
             if (!resultDir.exists()) {
                 resultDir.mkdir();
             }
-
             File file = new File("result/forecast.txt");
             PrintWriter writer = new PrintWriter(new FileWriter(file));
             String header = String.format("%-5s %-20s %-15s %-15s%n", "№", "Дата/время", "Температура (°C)", "Осадки (мм)");
